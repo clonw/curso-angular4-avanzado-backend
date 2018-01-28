5fbf4e492448dcdbb0cb094a3f2269014f786ac7
clonw <paquitol@gmail.com>
2018-01-28 01:35:01 +0100

    Capítulo 55

--- routes/user.js
+++ routes/user.js
@@ -7,11 +7,16 @@ var UserController = require('../controllers/user');
 var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
+var multipart = require('connect-multiparty');
+// dir donde se van a guardar kas imagenes de los usuarios
+var md_upload = multipart({uploadDir: './uploads/users'});
+
+
 //pasamos md_auth como midleware, se ejecutará antes que UserController.pruebas. además  md_auth.ensureAuth ejecutará el método interno next() para pasar a UserController.pruebas
 api.get('/pruebas-del-controlador',md_auth.ensureAuth, UserController.pruebas);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.login);
 //para actualozar algo de la BBDD
 api.put('/update-user/:id',md_auth.ensureAuth, UserController.updateUser);
-
+api.post('/upload-image-user/:id',[md_auth.ensureAuth, md_upload], UserController.uploadImage);
 module.exports = api;

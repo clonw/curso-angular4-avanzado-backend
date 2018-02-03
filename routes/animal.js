e890e8dc0848ca979bfa1d2400468e01ef47d862
clonw <paquitol@gmail.com>
2018-02-03 21:08:27 +0100

    Capitulo 65

--- routes/animal.js
+++ routes/animal.js
@@ -6,6 +6,7 @@ var AnimalController = require('../controllers/animal');
 //router de express
 var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
+var md_admin = require('../middlewares/is_admin');
 
 var multipart = require('connect-multiparty');
 // dir donde se van a guardar kas imagenes de los animales
@@ -14,13 +15,13 @@ var md_upload = multipart({uploadDir: './uploads/animals'});
 
 //pasamos md_auth como midleware, se ejecutará antes que UserController.pruebas. además  md_auth.ensureAuth ejecutará el método interno next() para pasar a UserController.pruebas
 api.get('/pruebas-animales',md_auth.ensureAuth, AnimalController.pruebas);
-api.post('/animal',md_auth.ensureAuth, AnimalController.saveAnimal);
+api.post('/animal',[md_auth.ensureAuth, md_admin.isAdmin], AnimalController.saveAnimal);
 api.get('/animals', AnimalController.getAnimals);
 api.get('/animal/:id',AnimalController.getAnimal);
-api.put('/animal/:id',md_auth.ensureAuth, AnimalController.updateAnimal);
-api.post('/upload-image-animal/:id',[md_auth.ensureAuth, md_upload], AnimalController.uploadImage);
+api.put('/animal/:id',[md_auth.ensureAuth, md_admin.isAdmin], AnimalController.updateAnimal);
+api.post('/upload-image-animal/:id',[md_auth.ensureAuth, md_admin.isAdmin,md_upload], AnimalController.uploadImage);
 api.get('/get-image-animal/:imageFile', AnimalController.getImageFile);
-api.delete('/animal/:id',AnimalController.deleteAnimal);
+api.delete('/animal/:id',[md_auth.ensureAuth, md_admin.isAdmin],AnimalController.deleteAnimal);
 
 
 module.exports = api;
